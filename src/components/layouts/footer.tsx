f8b0bacdd22f632206539a65c7cfff33eb9c1d52
jinew <jiyunk@naver.com>
2025-03-21 22:52:28 +0900

    fix:#2 copyright icon 삽입

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,11 +1,15 @@
+import { Copyright } from 'lucide-react';
 import React from 'react';
 
 const Footer = () => {
   return (
     <footer className='text-center bg-white py-8'>
-      <span className='text-gray text-md'>
-        ⓒ2025 오르조. All rights reserved.
-      </span>
+      <div className='flex items-center justify-center gap-1'>
+        <Copyright size={13} color='#71717A' strokeWidth={1} />
+        <span className='text-gray text-md'>
+          2025 오르조. All rights reserved.
+        </span>
+      </div>
     </footer>
   );
 };
