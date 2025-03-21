ac258ae0f6d69fc01a626164c7e49d01033fdaa8
taejin7088 <gog70888@gmail.com>
2025-03-21 20:34:27 +0900

    feat:#7 book-item 컴포넌트 기본 디자인 완료

--- src/types/books.type.ts
+++ src/types/books.type.ts
@@ -4,10 +4,15 @@ type Book = {
   author: string;
   published_date: string;
   rating: number;
-  price: number;
+  price: string;
   best_rank: number;
   publisher: string;
   description: string;
   genre: string;
   image_url: string;
 };
+
+type BookItem = Pick<
+  Book,
+  'title' | 'author' | 'rating' | 'price' | 'description' | 'image_url'
+>;
