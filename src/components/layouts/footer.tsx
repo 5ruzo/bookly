25478abf05496038b58c852cf591fd4a5587aca2
jinew <jiyunk@naver.com>
2025-03-21 22:11:31 +0900

    add:#2 footer 컴포넌트 추가

---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='text-center bg-white py-8'>
+      <span className='text-gray text-md'>
+        ⓒ2025 오르조. All rights reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
