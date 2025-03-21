622827ebaf9f58f99599411dbbda39aa0b015217
KGyeong <rlarudals61@naver.com>
2025-03-21 16:13:14 +0900

    chore: initial setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    localPatterns: [
+      {
+        pathname: "/public/images/**",
+        search: "",
+      },
+    ],
+  }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +10,26 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        gray: "var(--color-gray)",
+        "white-dark": "var(--color-white-dark)",
+        "white-light": "var(--color-white-light)",
+        black: "var(--color-black)",
+      },
+      fontFamily: {
+        sans: "var(--font-pretendard)",
+      },
+      fontSize: {
+        "2xl": "var(--text-2xl)",
+        xl: "var(--text-xl)",
+        lg: "var(--text-lg)",
+        md: "var(--text-md)",
+        sm: "var(--text-sm)",
+        ss: "var(--text-ss)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
