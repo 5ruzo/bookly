e1b497ab96a5cb948eb942b85066865ed04fed08
jinew <jiyunk@naver.com>
2025-03-21 22:10:37 +0900

    add:#2 header 우측 드롭다운 메뉴 컴포넌트 추가

---
+++ src/components/layouts/header-dropdown-menu.tsx
@@ -0,0 +1,39 @@
+import { HeaderMenuProps } from '@/types/layout.type';
+import Link from 'next/link';
+import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { AlignJustify } from 'lucide-react';
+
+const HeaderDropdownMenu: React.FC<HeaderMenuProps> = ({ menuList }) => {
+  return (
+    <div className='md:min-w-28 flex justify-end'>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <AlignJustify color='#71717A' strokeWidth={2.5} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='bg-white border-gray mr-4'>
+          {menuList.map((menu, index) => (
+            <DropdownMenuItem
+              key={index}
+              className='cursor-pointer text-md'
+              onClick={menu.onClick}
+            >
+              {menu.href ? (
+                <Link href={menu.href}>{menu.text}</Link>
+              ) : (
+                <span>{menu.text}</span>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default HeaderDropdownMenu;
