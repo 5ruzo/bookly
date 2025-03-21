e4455165002587bb349351a5ad2ee210634f66f7
taejin7088 <gog70888@gmail.com>
2025-03-21 19:05:06 +0900

    add:#7 books 페이지에 쓰일 타입정의하는 파일

---
+++ src/types/books.type.ts
@@ -0,0 +1,13 @@
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  published_date: string;
+  rating: number;
+  price: number;
+  best_rank: number;
+  publisher: string;
+  description: string;
+  genre: string;
+  image_url: string;
+};
