810a501598c9d9118bafeed307439287e241b6cd
taejin7088 <taegin70888@gmail.com>
2025-03-23 01:40:50 +0900

    chore:#7 작업한 파일들 주석 추가

--- src/lib/api/book-list.api.ts
+++ src/lib/api/book-list.api.ts
@@ -1,6 +1,7 @@
 import { BOOK_LIST_REVALIDATE_TIME } from '@/constants/book-list';
 
 export const fetchGetBookListByTop50Rank = async () => {
+  //베스트랭킹50개 책 목록을 가져옴(supabase 메서드로 리펙터링 예정)
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/books?best_rank=lte.50`,
     {
@@ -15,10 +16,9 @@ export const fetchGetBookListByTop50Rank = async () => {
       },
     }
   );
-
   const data: Book[] = await res.json();
 
-  console.log(data);
+  //가져온 데이터 랭킹순으로 오름차순 정렬
   data.sort((a, b) => a.best_rank - b.best_rank);
 
   return data;
