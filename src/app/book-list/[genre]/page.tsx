a8fa4166431ceba1328ed53b5cee941e7fbfda30
taejin7088 <taegin70888@gmail.com>
2025-03-23 20:56:00 +0900

    feat:#7 book-list 레이아웃 구성(장르 목록은 목데이터, [genre]페이지 테스트페이지)

---
+++ src/app/book-list/[genre]/page.tsx
@@ -0,0 +1,8 @@
+import { BOOK_LIST_REVALIDATE_TIME } from '@/constants/book-list';
+import React from 'react';
+
+export const revalidate = BOOK_LIST_REVALIDATE_TIME;
+
+export default async function BookListGenrePage() {
+  return <div>테스트</div>;
+}
--- src/components/features/book-list/book-list-best-sidebar.tsx
+++ src/components/features/book-list/book-list-best-sidebar.tsx
@@ -1,5 +1,39 @@
 'use client';
 
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+
+const genreArray = [
+  '만화',
+  '소설',
+  '자기계발',
+  '에세이',
+  '경제',
+  '사회과학',
+  '좋은부모',
+];
+
 export default function BookListBestSidebar() {
-  return <>야야</>;
+  const params = useParams();
+  const genre = params.genre && decodeURIComponent(params.genre as string);
+
+  return (
+    <nav>
+      <ul>
+        {genreArray.map((genreItem) => {
+          const bgColor = genreItem === genre && 'bg-secondary';
+          return (
+            <Link href={`/book-list/${genreItem}`}>
+              <li
+                key={genreItem}
+                className={`${bgColor} text-sm w-[162px] h-[26px] pl-[24px] flex items-center`}
+              >
+                {genreItem}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
