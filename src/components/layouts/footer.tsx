9c9dc3596a72dd537ac34319fe2f449a924597cb
jinew <jiyunk@naver.com>
2025-03-24 10:55:12 +0900

    style:#2 푸터에 border-top 추가

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='text-center bg-white py-8'>
+    <footer className='text-center bg-white py-8 border-lightgray border-t-[1px]'>
       <div className='flex items-center justify-center gap-1'>
         <Copyright size={13} color='var(--color-gray)' strokeWidth={1} />
         <span className='text-gray text-md'>
