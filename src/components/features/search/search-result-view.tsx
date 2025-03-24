433e8e966e5205800eac1773e2960aba3bb57de0
taejin7088 <taegin70888@gmail.com>
2025-03-24 23:18:03 +0900

    feat:#31 쿼리스트링으로 받아오는걸로 데이터 검새

--- src/components/features/search/search-result-view.tsx
+++ src/components/features/search/search-result-view.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-import { fetchGetBookListByTop50Rank } from '@/lib/api/book-list.api';
 import { useEffect, useState } from 'react';
 import BookListItem from '../book-list/book-list-item';
+import { fetchGetSearchedBookList } from '@/lib/api/search.api';
+import { useSearchParams } from 'next/navigation';
 
 export default function SearchResultView() {
   const [bookList, setBookList] = useState<Book[]>([]);
 
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get('query');
+
   useEffect(() => {
-    fetchGetBookListByTop50Rank().then((data) => {
-      setBookList(data);
-    });
-  }, []);
+    if (searchTerm) {
+      fetchGetSearchedBookList(searchTerm).then((data) => {
+        setBookList(data);
+      });
+    }
+  }, [searchTerm]);
 
   return (
     <ul className='flex flex-col gap-[70px]'>
