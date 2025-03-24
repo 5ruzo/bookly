20b63442e0d89eaef611d3e742f5cb37f64d029f
KGyeong <rlarudals61@naver.com>
2025-03-24 23:12:38 +0900

    style:#3 heart icon size up

--- src/components/features/detail/button-area/detail-like-button.tsx
+++ src/components/features/detail/button-area/detail-like-button.tsx
@@ -2,23 +2,25 @@
 
 import { useState } from 'react';
 import { Heart } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 
 export default function LikeButton() {
   const [isFavorite, setIsFavorite] = useState(false);
 
   return (
-    <Button
-      variant='outline'
-      className={`${isFavorite ? 'bg-primary text-white border-primary' : 'text-primary border-primary'} rounded-xl h-12 w-12 md:h-14 md:w-14 p-0 flex items-center justify-center`}
+    <button
+      className={`${isFavorite ? 'text-red-500 border-primary' : 'text-primary border-primary'} focus:outline-none border-[1px] rounded-xl h-12 w-12 md:h-14 md:w-14 p-0 flex items-center justify-center`}
       onClick={() => {
         setIsFavorite(!isFavorite);
         if (isFavorite) alert('찜 해제!');
         if (!isFavorite) alert('찜!');
       }}
     >
-      <Heart className={`h-6 w-6 ${isFavorite ? 'fill-white' : ''}`} />
+      <Heart
+        size={24}
+        strokeWidth={1}
+        className={` ${isFavorite ? 'fill-red-500' : ''}`}
+      />
       <span className='sr-only'>좋아요</span>
-    </Button>
+    </button>
   );
 }
