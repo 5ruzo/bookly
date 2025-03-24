08fd56e2e6d7833a1c3390ba3060b8a917f6b921
taejin7088 <taegin70888@gmail.com>
2025-03-24 23:29:51 +0900

    feat:#31 search 로딩컴포넌트 추가

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,4 +1,4 @@
-export default function BookListLoading() {
+export default function SearchLoading() {
   return (
     //메인 컨텐츠 영역이 회색 박스로 처리
     <div className='w-[940px] h-[1500px] bg-gray animate-pulse rounded'></div>
