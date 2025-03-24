9a63f4b95102d550fec9a5f2eb6af05faff156c2
KGyeong <rlarudals61@naver.com>
2025-03-24 19:18:01 +0900

    refactor:#3 devide component for style

---
+++ src/components/features/detail/detail-product-info.tsx
@@ -0,0 +1,52 @@
+'use client';
+import { formatNumber } from '@/lib/utils/detail/format-number';
+import { ProductInfoProps } from '@/types/detail.type';
+import { Rating } from './detail-rating';
+import ButtonArea from './detail-button-area';
+import { useState } from 'react';
+import QuantityInput from './detail-quantity-input';
+
+export default function ProductInfo({ data, price }: ProductInfoProps) {
+  const [quantity, setQuantity] = useState(1);
+
+  // 수령 예상일 계산을 위한 기본값
+  const timestamp = Date.now();
+  const date = new Date(timestamp);
+
+  return (
+    <div className='content-center'>
+      <div className='flex gap-3 md:gap-16 h-[300px] md:h-3/5 w-full'>
+        <div className='grid text-md md:text-lg content-around'>
+          <h2>가격</h2>
+          <h2>배송료</h2>
+          <h2>수령 예상일</h2>
+          <h2>평점</h2>
+          <h2>수량</h2>
+        </div>
+        <div className='grid text-md md:text-lg content-around'>
+          <p>{formatNumber(Number(data.price))}원</p>
+          <div>
+            <span>3,000원</span>
+            <span className='text-md text-gray'>(제주도 6,000원)</span>
+          </div>
+          <p>
+            {/* 오늘을 기준으로 3일 이후 */}
+            {new Date(date.setDate(date.getDate() + 3)).toLocaleDateString()}
+          </p>
+          <div className='h-4 md:h-full'>
+            <Rating rating={data.rating} />
+            <span className='sr-only'>별점 {data.rating}점</span>
+          </div>
+          <QuantityInput
+            value={quantity}
+            onChange={setQuantity}
+            price={price}
+          />
+        </div>
+      </div>
+      <div className='mt-4 md:mt-16'>
+        <ButtonArea title={data.title} price={data.price} quantity={quantity} />
+      </div>
+    </div>
+  );
+}
