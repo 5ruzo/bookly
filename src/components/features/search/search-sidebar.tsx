7c3d65922fa58fef952e332f79792f251ba5f949
taejin7088 <taegin70888@gmail.com>
2025-03-25 10:33:36 +0900

    chore:#31 주석삭제

--- src/components/features/search/search-sidebar.tsx
+++ src/components/features/search/search-sidebar.tsx
@@ -9,29 +9,3 @@ export default function SearchSidebar({ genreList }: { genreList: string[] }) {
     </aside>
   );
 }
-
-// <nav>
-// <ul>
-//   {genreList.map((genreItem) => {
-//     //장르 구분자가 '/'로 들어가면 각 구분 페이지로 인식되기 때문에, 이를 '-'로 변경하여 처리
-//     const formattedGenreForUrl = genreItem.replace(/\//g, '-');
-
-//     const bgColor =
-//       genreItem === genreFromPath.replace(/-/g, '/') && 'bg-secondary';
-//     return (
-//       <Link
-//         key={genreItem}
-//         href={`/book-list/${formattedGenreForUrl}`}
-//         replace={!!params.genre}
-//       >
-//         <li
-//           key={genreItem}
-//           className={`${bgColor} text-sm w-[162px] h-[26px] pl-[24px] flex items-center`}
-//         >
-//           {genreItem}
-//         </li>
-//       </Link>
-//     );
-//   })}
-// </ul>
-// </nav>
