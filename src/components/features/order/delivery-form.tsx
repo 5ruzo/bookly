c6801ba60be3521ce8b5b0cf80a75dd210144cd2
hye10 <dhye410@daum.net>
2025-03-25 23:16:42 +0900

    refactor:#24 배송정보 Form Field 공통 컴포넌트 분리

--- src/components/features/order/delivery-form.tsx
+++ src/components/features/order/delivery-form.tsx
@@ -4,23 +4,12 @@ import { Button } from '@/components/ui/button';
 import { useDeliveryFormHook } from '@/lib/hooks/order/delivery-form-hook';
 import { TypeAddressInfo, TypeOrderForm } from '@/types/order/order.type';
 import { FieldValues, FormState, UseFormRegister } from 'react-hook-form';
+import FormField, { formStyles } from './form-field';
 
 type DeliveryFormProps<T extends FieldValues> = {
   register: UseFormRegister<T>;
   formState: FormState<T>;
   onChangeAddress: (addressInfo: TypeAddressInfo) => void;
-};
-
-const styles = {
-  formField: 'w-full mb-3 flex items-center flex-wrap flex-col sm:flex-row',
-  label: 'w-full font-semibold text-left sm:text-center sm:w-[20%]',
-  input:
-    'w-full text-[0.8rem] sm:text-[1rem] py-2 indent-2 rounded-xl bg-secondary sm:w-[80%]',
-  addressInput:
-    'w-[70%] bg-secondary indent-2 py-2 rounded-xl text-[0.8rem] sm:text-[1rem]',
-  button: 'rounded-xl ml-1 w-[40%]',
-  error:
-    'ml-[2%] h-[20px] text-[0.7rem] mt-1 w-full sm:text-[0.8rem] font-semibold text-red-500 sm:ml-[21%]',
 };
 
 export default function DeliveryForm({
@@ -41,40 +30,25 @@ export default function DeliveryForm({
 
   return (
     <div>
-      <div className={styles.formField}>
-        <label htmlFor='name' className={styles.label}>
-          수령인
-        </label>
-        <input
-          id='name'
-          type='text'
-          className={styles.input}
-          placeholder='이름을 입력해 주세요.'
-          {...register('name')}
-        />
-        <p className={styles.error}>{formState?.errors?.name?.message}</p>
-      </div>
+      <FormField
+        id='name'
+        label='수령인'
+        register={register}
+        formState={formState}
+        placeholder='이름을 입력해 주세요.'
+      />
 
-      <div className={styles.formField}>
-        <label htmlFor='phoneNumber' className={styles.label}>
-          핸드폰 번호
-        </label>
-        <input
-          id='phoneNumber'
-          type='text'
-          className={styles.input}
-          placeholder='숫자만 입력해 주세요.(01012345678)'
-          {...register('phoneNumber')}
-        />
-
-        <p className={styles.error}>
-          {formState?.errors?.phoneNumber?.message}
-        </p>
-      </div>
+      <FormField
+        id='phoneNumber'
+        label='핸드폰 번호'
+        register={register}
+        formState={formState}
+        placeholder='숫자만 입력해 주세요.(01012345678)'
+      />
 
       <div className='w-full mb-3'>
         <div className={`flex items-center flex-col sm:flex-row`}>
-          <label htmlFor='address' className={styles.label}>
+          <label htmlFor='address' className={formStyles.label}>
             배송지 주소
           </label>
           <div className='flex items-center w-full sm:w-[80%]'>
@@ -82,49 +56,38 @@ export default function DeliveryForm({
               id='address'
               type='text'
               readOnly
-              className={styles.addressInput}
+              className='w-[70%] bg-secondary indent-2 py-2 rounded-xl text-[0.8rem] sm:text-[1rem]'
               placeholder='주소를 검색해 주세요.'
               {...register('address')}
             />
-            <Button className={styles.button} onClick={getAddressInfo}>
+            <Button
+              className='rounded-xl ml-1 w-[40%]'
+              onClick={getAddressInfo}
+            >
               주소 검색
             </Button>
           </div>
         </div>
-        <p className={`${styles.error}`}>
+        <p className={formStyles.error}>
           {formState?.errors?.address?.message}
         </p>
       </div>
 
-      <div className={styles.formField}>
-        <label htmlFor='detailAddress' className={styles.label}>
-          상세주소
-        </label>
-        <input
-          id='detailAddress'
-          type='text'
-          className={styles.input}
-          placeholder='상세주소를 입력해 주세요.'
-          {...register('detailAddress')}
-        />
-        <p className={styles.error}>
-          {formState?.errors?.detailAddress?.message}
-        </p>
-      </div>
-      <div className={styles.formField}>
-        <label htmlFor='zoneCode' className={styles.label}>
-          우편번호
-        </label>
-        <input
-          id='zoneCode'
-          readOnly={true}
-          className={styles.input}
-          placeholder='주소를 선택하면 우편번호가 자동으로 들어갑니다.'
-          type='text'
-          {...register('zoneCode')}
-        />
-        <p className={styles.error}>{formState?.errors?.zoneCode?.message}</p>
-      </div>
+      <FormField
+        id='detailAddress'
+        label='상세주소'
+        register={register}
+        formState={formState}
+        placeholder='상세주소를 입력해 주세요.'
+      />
+
+      <FormField
+        id='zoneCode'
+        label='상세주소'
+        register={register}
+        formState={formState}
+        placeholder='주소를 선택하면 우편번호가 자동으로 들어갑니다.'
+      />
     </div>
   );
 }
