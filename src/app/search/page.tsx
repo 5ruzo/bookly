90892e7baa2943873a3075be84dd3cc75a8d5c36
taejin7088 <taegin70888@gmail.com>
2025-03-25 12:40:58 +0900

    feat:#31 쿼리스트링으로 검색결과 메타데이터 처리

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,18 +2,19 @@ import SearchResultView from '@/components/features/search/search-result-view';
 import React from 'react';
 
 export async function generateMetadata({
-  params,
+  searchParams,
 }: {
-  params: { genre: string };
+  searchParams: { query: string };
 }) {
-  const searchString = '해당책';
+  const searchString = searchParams.query;
+
   return {
-    title: ` ${searchString} 검색결과 - Bookly에서 책 찾아보기`,
-    description: `Bookly에서 '${searchString}의 검색결과를 확인하세요.`,
+    title: ` '${searchString}' 책 제목 검색결과 - Bookly에서 책 찾아보기`,
+    description: `Bookly에서 '${searchString}'의 검색결과를 확인하세요.`,
 
     openGraph: {
       title: `Bookly - '${searchString}'에 대한 검색 결과 확인하기`,
-      description: `Bookly에서 '${searchString}의 검색결과를 확인하세요.`,
+      description: `Bookly에서 '${searchString}'의 검색결과를 확인하세요.`,
       images: '/images/logo.png',
     },
   };
