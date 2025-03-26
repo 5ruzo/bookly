f0344c7b9d14c5eb2e72d58793050b2d56b31d87
hye10 <dhye410@daum.net>
2025-03-26 22:16:48 +0900

    fix:#53 공통 book-card 장바구니 담기 버튼 기능 추가

--- src/components/ui/book-card.tsx
+++ src/components/ui/book-card.tsx
@@ -2,9 +2,9 @@ import {
   formatNumberWithCommas,
   normalizedRating,
 } from '@/lib/utils/common.util';
+import useCartStore from '@/store/cart-store';
 import { CardForCarousel } from '@/types/common.type';
 import { ShoppingCart, Star } from 'lucide-react';
-import Link from 'next/link';
 
 function BookCard({
   id,
@@ -15,42 +15,59 @@ function BookCard({
   description,
   price,
 }: CardForCarousel) {
+  const { addToCart } = useCartStore();
+  const handleAddToCart = () => {
+    addToCart([
+      {
+        id,
+        quantity: 1,
+        price: Number(price),
+        bookInfo: {
+          image_url,
+          title,
+          author,
+        },
+      },
+    ]);
+    window.alert('장바구니에 추가되었습니다.');
+  };
   return (
-    <Link href={`/detail/${id}`}>
-      <div className='border-[1px] border-lightgray rounded-md overflow-hidden pb-4'>
-        <div className='relative w-full max-w-1/3 aspect-square overflow-hidden'>
-          <img
-            src={image_url}
-            alt={title}
-            className='absolute object-cover object-top'
-          />
-        </div>
-        <div className='flex flex-col gap-2 mt-2 px-4'>
-          <span className='flex items-center text-md gap-1'>
-            <Star className='w-5' fill='#FACC15' stroke='none' />
-            {normalizedRating(rating)}
-          </span>
-          <span className='font-semibold text-md block w-full overflow-hidden text-ellipsis whitespace-nowrap'>
-            {title}
-          </span>
-          <span className='text-sm text-gray overflow-hidden text-ellipsis whitespace-nowrap'>
-            {author}
-          </span>
-          <div className='h-12'>
-            <p className='text-md text-gray line-clamp-2 break-words text-ellipsis overflow-hidden'>
-              {description}
-            </p>
-          </div>
-          <span className='text-md font-semibold'>
-            {formatNumberWithCommas(Number(price))}원
-          </span>
-          <button className='flex justify-center gap-1 items-center mt-1 text-sm md:text-md bg-black text-white-dark py-1 rounded-md'>
-            <ShoppingCart fill='var(--color-white-dark)' className='w-4' />
-            장바구니에 담기
-          </button>
-        </div>
+    <div className='border-[1px] border-lightgray rounded-md overflow-hidden pb-4'>
+      <div className='relative w-full max-w-1/3 aspect-square overflow-hidden'>
+        <img
+          src={image_url}
+          alt={title}
+          className='absolute object-cover object-top'
+        />
       </div>
-    </Link>
+      <div className='flex flex-col gap-2 mt-2 px-4'>
+        <span className='flex items-center text-md gap-1'>
+          <Star className='w-5' fill='#FACC15' stroke='none' />
+          {normalizedRating(rating)}
+        </span>
+        <span className='font-semibold text-md block w-full overflow-hidden text-ellipsis whitespace-nowrap'>
+          {title}
+        </span>
+        <span className='text-sm text-gray overflow-hidden text-ellipsis whitespace-nowrap'>
+          {author}
+        </span>
+        <div className='h-12'>
+          <p className='text-md text-gray line-clamp-2 break-words text-ellipsis overflow-hidden'>
+            {description}
+          </p>
+        </div>
+        <span className='text-md font-semibold'>
+          {formatNumberWithCommas(Number(price))}원
+        </span>
+        <button
+          className='flex justify-center gap-1 items-center mt-1 text-sm md:text-md bg-black text-white-dark py-1 rounded-md'
+          onClick={handleAddToCart}
+        >
+          <ShoppingCart fill='var(--color-white-dark)' className='w-4' />
+          장바구니에 담기
+        </button>
+      </div>
+    </div>
   );
 }
 
