01d63e92cf96dbd1c52728446d2ed1295a16486e
taejin7088 <taegin70888@gmail.com>
2025-03-26 20:59:52 +0900

    design:#7 검색결과가 없습니다 페이지 수정

--- src/components/features/search/search-no-search-result.tsx
+++ src/components/features/search/search-no-search-result.tsx
@@ -6,16 +6,19 @@ import RecommendedBooksCarousel from '@/components/ui/recommened-books-carousel'
 
 export default function SearchNoSearchResult() {
   return (
-    <section className='w-[1000px] min-h-[calc(100vh-24rem)] pl-1 flex flex-col justify-start items-center'>
+    <section className='min-h-[calc(100vh-24rem)] pl-1 flex flex-col justify-start items-center'>
       <h2 className='text-2xl'>검색결과가 없습니다.</h2>
       <CardSectionLayout
         title='베스트셀러'
         path='/book-list/best'
-        className='mt-20'
+        className='mt-20 w-screen px-4'
       >
         <BestSellerCarousel />
       </CardSectionLayout>
-      <CardSectionLayout title='이 주의 추천 도서' className='my-20'>
+      <CardSectionLayout
+        title='이 주의 추천 도서'
+        className='my-20  w-screen px-4'
+      >
         <RecommendedBooksCarousel />
       </CardSectionLayout>
     </section>
--- src/components/layouts/side-layout.tsx
+++ src/components/layouts/side-layout.tsx
@@ -15,21 +15,21 @@ export default function SideLayout({
     <main className='flex justify-center mt-[50px] gap-[71px]'>
       <div className='max-w-[1200px] flex justify-between'>
         {/* 작은사이즈일때 나타나는 사이드바 햄버거 */}
-        <aside className='md:hidden fixed top-[100px] left-0 z-50 bg-gray rounded-r-lg p-4'>
+        <aside className='lg:hidden fixed top-[100px] left-0 z-50 bg-gray rounded-r-lg p-4'>
           <SideDropdownMenu>
             <SidebarContent />
           </SideDropdownMenu>
         </aside>
         {/* 보통사이즈일때 나타나는 사이드바 */}
 
-        <aside className='hidden md:block'>
+        <aside className='hidden lg:block'>
           <div className='w-[194px]'>
             <div className='fixed border-t border-b border-solid px-[16px] py-[20px]'>
               <SidebarContent />
             </div>
           </div>
         </aside>
-        <section className='w-auto md:w-2/3'>{children}</section>
+        <section className='w-auto lg:w-2/3'>{children}</section>
       </div>
     </main>
   );
