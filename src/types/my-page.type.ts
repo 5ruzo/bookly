a098d7b118646c50c32564f44d2e0df56136d6fe
KGyeong <rlarudals61@naver.com>
2025-03-26 18:32:27 +0900

    feat:#9 주문 내역 타입 선언

--- src/types/my-page.type.ts
+++ src/types/my-page.type.ts
@@ -1,9 +1,39 @@
 export type OrderDetailInfo = {
   id: number;
+  created_at: string;
+  user_id: string;
+  total_price: number;
+  order_details: OrderedBookInfoArray;
+};
+
+export type OrderedBookInfo = {
+  id: number;
+  books: {
+    id: number;
+    genre: string;
+    price: string;
+    title: string;
+    author: string;
+    rating: number;
+    best_rank: number;
+    image_url: string;
+    publisher: string;
+    description: string;
+    published_date: string;
+  };
+  book_id: number;
+  order_id: number;
+  quantity: number;
+};
+
+type OrderedBookInfoArray = OrderedBookInfo[];
+
+export type OrderDetailCardProps = {
+  id: number;
   image_url: string;
   title: string;
   author: string;
-  arrived: boolean;
+  arrived: string;
   price: string;
   quantity: number;
 };
