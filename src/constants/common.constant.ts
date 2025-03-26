2ce21f463ed1912012434bec77e4fec5ddee53f9
hye10 <dhye410@daum.net>
2025-03-26 14:16:14 +0900

    fix:#24 충돌 해결 시 누락되었던 부분 추가

--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -1,6 +1,9 @@
 // 스크롤 한 번에 슬라이드 될 개수
-export const SLIDES_TO_ONE_SCROLL = 4;
-
+export const SLIDES_TO_ONE_SCROLL = {
+  SMALL: 2,
+  MEDIUM: 3,
+  LARGE: 4,
+};
 export const QUERY_KEYS = {
   BEST_SELLER: ['BEST_SELLER'],
   RECOMMENDED_BOOKS: ['RECOMMENDED_BOOKS'],
